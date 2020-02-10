947d2afc6246cd8c8fd4592501870c781b6a11fe
lizz <lizz@sensetime.com>
2020-02-10 23:31:47 +0800

    bench

--- config/bench.py
+++ config/bench.py
@@ -75,7 +75,7 @@ test_pipeline = [
 ]
 data = dict(
     videos_per_gpu=8,
-    workers_per_gpu=4,
+    workers_per_gpu=0,
     train=dict(
         type=dataset_type,
         ann_file=ann_file_train,
